3811dc80352bd6c6fa03fd51278768d294f291b5
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-01 19:16:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,10 @@
 class Show < ActiveRecord::Base
 
   def highest_rating
-    Show.order(:desc).first
+    Show.where("rating").maximum
   end
 
 end
+
+#
+# .where("release_date > 2002").order(release_date: :desc)
